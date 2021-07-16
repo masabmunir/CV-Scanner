3f335849e29e089177eaa2ebdd42dc0538e3017f
Masab <Masabk214@gmail.com>
2021-07-16 07:59:24 -0700

    adding data to js file

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 console.log("working");
+
+const data = [
+    {
+        name: 'MAsab',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    },
+
+    {
+        name: 'Ali',
+        age: 28,
+        city: 'Bangalore',
+        language: 'JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/54.jpg'
+    },
+
+    {
+        name: 'Hina',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/women/55.jpg'
+    },
+
+    {
+        name: 'Alina Ali',
+        age: 45,
+        city: 'Mumbai',
+        language: 'Python',
+        framework: 'Flask',
+        image: 'https://randomuser.me/api/portraits/women/57.jpg'
+    },
+
+    {
+        name: 'Rohit Sharma',
+        age: 34,
+        city: 'Jharkhand',
+        language: 'Go',
+        framework: 'Go Framework',
+        image: 'https://randomuser.me/api/portraits/men/61.jpg'
+    }
+
+]
+
+
